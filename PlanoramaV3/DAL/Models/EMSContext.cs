c876c3412e1e5d3627c55e52fb0f0f920e26aa00
Atia Rasida Silvia <atiasilvia0@gmail.com>
2024-01-01 03:00:31 +0600

    EMS contex added

---
+++ PlanoramaV3/DAL/Models/EMSContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace DAL.Models
+{
+    public class EMSContext : DbContext
+    {
+        public DbSet<FoodsCategory> FoodsCategories { get; set;}
+        public DbSet<Food> Foods { get; set;}
+        public DbSet<DecorationsCategory> DecorationsCategories {  get; set;}
+        public DbSet<Dacoration> Decorations { get; set; }
+        public DbSet<LocationsCategory> LocationsCategories { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }
+}
