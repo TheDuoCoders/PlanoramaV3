498b437400a475c1e4a1211e071a7a187ea4f4c4
Mohammad Abdur Rouf Hrithik <rroufislam@yahoo.com>
2024-01-01 02:38:39 +0600

    user class

---
+++ PlanoramaV3/DAL/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Models
+{
+    public class User
+    {
+        [Key]
+        public string Uname { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Password { get; set; }
+
+        [Required]
+        public DateTime DOB { get; set; }    
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Address { get; set; }    
+
+        [Required]
+        public int Points { get; set; }
+
+        [Required]
+        public string UserType {get; set;}
+    }
+}
