c6e12e1f0064d9391900a8a67b738b15801dadcf
Atia Rasida Silvia <atiasilvia0@gmail.com>
2024-01-01 02:44:05 +0600

    food added

---
+++ PlanoramaV3/DAL/Models/Food.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Models
+{
+    public class Food
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+
+        public string Name { get; set; }
+
+        [ForeignKey("FoodsCategory")]
+        public int FoodCategory {  get; set; }
+
+        [Required]
+        [StringLength(20)]
+
+        public string Picture { get; set; }
+
+        [Required]
+        [StringLength(200)]
+
+        public string Description { get; set; }
+
+        [Required]
+
+        public double Price { get; set; }
+
+        [Required]
+
+        public int TimesSelected { get; set; }
+
+        public virtual FoodsCategory FoodsCategory { get; set; }
+    }
+}
--- PlanoramaV3/DAL/Models/FoodsCategory.cs
+++ PlanoramaV3/DAL/Models/FoodsCategory.cs
@@ -1,7 +1,26 @@
-﻿namespace DAL.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Models
 {
     public class FoodsCategory
     {
-        
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        public virtual ICollection<Food> Foods { get; set; }
+
+        public FoodsCategory()
+        {
+            Foods = new List<Food>();
+        }
     }
 }
