176b2891e5d3c5a45fa0661a1768fd8eefbfc463
Mohammad Abdur Rouf Hrithik <rroufislam@yahoo.com>
2024-01-01 02:56:41 +0600

    Booking Added

---
+++ PlanoramaV3/DAL/Models/Booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Models
+{
+    public class Booking
+    {   //not done - need package
+        public int Id { get; set; }
+
+        [Required]
+        public int PackageId { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public string OrderedBy { get; set; }    
+
+        [Required]
+        public DateTime OrderTime { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
