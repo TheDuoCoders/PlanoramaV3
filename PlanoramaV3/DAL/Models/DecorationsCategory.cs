a47c334dbe9ed4d946f474b09321a56dabaf9fd5
Atia Rasida Silvia <atiasilvia0@gmail.com>
2024-01-01 02:50:44 +0600

    commit all class

---
+++ PlanoramaV3/DAL/Models/DecorationsCategory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Models
+{
+    public class DecorationsCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        public virtual ICollection<Dacoration> Decorations { get; set; }
+
+        public DecorationsCategory()
+        {
+            Decorations = new List<Dacoration>();
+        }
+    }
+}
